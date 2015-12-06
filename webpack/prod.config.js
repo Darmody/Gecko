66bf11f9b44dfc9c8d778911f9b067169da554d2
Darmody <eterlf41@gmail.com>
2015-12-06 18:18:50 +0800

    add gecko icon

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -37,7 +37,7 @@ var config = {
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.(svg|png)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
     ]
   },
   progress: true,
